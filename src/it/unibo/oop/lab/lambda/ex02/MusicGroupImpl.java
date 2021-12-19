06f3d530f18b1de34120b8b5937a785f8d754b94
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-19 14:22:27 +0100

    feat: implement longestAlbum()

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -1,5 +1,6 @@
 package it.unibo.oop.lab.lambda.ex02;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -74,7 +75,11 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Optional<String> longestAlbum() {
-        return null;
+        return this.songs.stream().collect(Collectors.groupingBy(Song::getAlbumName, Collectors.summingDouble(Song::getDuration)))
+                                  .entrySet()
+                                  .stream()
+                                  .collect(Collectors.maxBy(Comparator.comparingDouble(Entry::getValue)))
+                                  .flatMap(Entry::getKey);
     }
 
     private static final class Song {
